a11b0f09a059c1680989e13b883f5b018532214a
Prajwal M <prajwalmallapur123@gmail.com>
2024-04-28 11:42:33 +0530

    Shopping

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css' 
+import MainPage from './shopping/pages/MainPage'
+
+
+const App = () => {
+  return (
+    <div>
+     
+      <MainPage />
+   
+     
+    </div>
+  )
+}
+
+export default App
